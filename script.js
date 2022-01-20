573430ad4aa8763c52c9140cae439734269dc220
gefgu <gefgu@hotmail.com>
2022-01-20 11:05:24 -0300

    Add library and option to add to it

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function Book(title, author, numberOfPages, readingDone) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.readingDone = readingDone;
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.readingDone = readingDone;
+}
+
+let library = [];
+
+function addBookToLibrary(book) {
+  library.push(book);
 }
