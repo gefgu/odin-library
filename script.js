0ac93af7fa72e58cef02e0026fd1d4d3200a3d63
gefgu <gefgu@hotmail.com>
2022-01-20 16:43:41 -0300

    Add clean display functionality and formatting to js file

--- script.js
+++ script.js
@@ -12,33 +12,41 @@ function addBookToLibrary(book) {
 }
 
 function displayLibrary() {
-    const container = document.querySelector(".container")
-    library.forEach(book => {
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("book-card");
-        const bookTitle = document.createElement("h3");
-        const bookAuthor = document.createElement("p");
-        const numPages = document.createElement("p");
-        const readingStatus = document.createElement("p");
+  library.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const numPages = document.createElement("p");
+    const readingStatus = document.createElement("p");
 
-        bookTitle.textContent = book.title;
-        bookAuthor.textContent = `Author: ${book.author}`;
-        numPages.textContent = `Number of Pages: ${book.numberOfPages}`;
-        if (book.readingDone) {
-            readingStatus.textContent = "Reading Complete!";
-        } else {
-            readingStatus.textContent = "Not read yet!";
-        }
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = `Author: ${book.author}`;
+    numPages.textContent = `Number of Pages: ${book.numberOfPages}`;
+    if (book.readingDone) {
+      readingStatus.textContent = "Reading Complete!";
+    } else {
+      readingStatus.textContent = "Not read yet!";
+    }
 
-        bookCard.appendChild(bookTitle);
-        bookCard.appendChild(bookAuthor);
-        bookCard.appendChild(numPages);
-        bookCard.appendChild(readingStatus);
-        container.appendChild(bookCard);
-    })
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(numPages);
+    bookCard.appendChild(readingStatus);
+    container.appendChild(bookCard);
+  });
 }
 
+function cleanDisplay() {
+  container.childNodes.forEach(e => e.remove())
+}
+
+
+const container = document.querySelector(".container");
+
 addBookToLibrary(new Book("The Final Empire", "Brandon Sanderson", 541, true));
-addBookToLibrary(new Book("The Well of Ascension", "Brandon Sanderson", 590, true));
+addBookToLibrary(
+  new Book("The Well of Ascension", "Brandon Sanderson", 590, true)
+);
 addBookToLibrary(new Book("The Hero of Ages", "Brandon Sanderson", 572, false));
 displayLibrary();
