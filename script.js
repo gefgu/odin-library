7c7868ad96f1c49dedbda32fce6c11b890324617
gefgu <gefgu@hotmail.com>
2022-01-20 11:25:12 -0300

    Add function to display books from library into DOM

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ function addBookToLibrary(book) {
   library.push(book);
 }
 
+function displayLibrary() {
+    const container = document.querySelector(".container")
+    library.forEach(book => {
+        const bookCard = document.createElement("div");
+        const bookTitle = document.createElement("h3");
+        const bookAuthor = document.createElement("p");
+        const numPages = document.createElement("p");
+        const readingStatus = document.createElement("p");
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        numPages.textContent = `Number of Pages: ${book.numberOfPages}`;
+        if (book.readingDone) {
+            readingStatus.textContent = "Reading Complete!";
+        } else {
+            readingStatus.textContent = "Not read yet";
+        }
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(numPages);
+        bookCard.appendChild(readingStatus);
+        container.appendChild(bookCard);
+    })
+}
+
 addBookToLibrary(new Book("The Final Empire", "Brandon Sanderson", 541, true));
 addBookToLibrary(new Book("The Well of Ascension", "Brandon Sanderson", 590, true));
 addBookToLibrary(new Book("The Hero of Ages", "Brandon Sanderson", 572, true));
+displayLibrary();
