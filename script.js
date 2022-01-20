61928a1c5021dbdedbf776c1f3bb121861d4e31a
gefgu <gefgu@hotmail.com>
2022-01-20 11:09:44 -0300

    Add sample books

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ let library = [];
 function addBookToLibrary(book) {
   library.push(book);
 }
+
+addBookToLibrary(new Book("The Final Empire", "Brandon Sanderson", 541, true));
+addBookToLibrary(new Book("The Well of Ascension", "Brandon Sanderson", 590, true));
+addBookToLibrary(new Book("The Hero of Ages", "Brandon Sanderson", 572, true));
