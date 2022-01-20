db0e0c7a84e5300bf319558c0358008302493de2
gefgu <gefgu@hotmail.com>
2022-01-20 11:03:49 -0300

    Add Book constructor

---
+++ script.js
@@ -0,0 +1,6 @@
+function Book(title, author, numberOfPages, readingDone) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.readingDone = readingDone;
+}
