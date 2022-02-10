923820d2b4d72f2a782410727d9bdc66524b603f
gefgu <gefgu@hotmail.com>
2022-02-10 20:00:49 -0300

    Add Book declarition using classes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function Book(title, author, numberOfPages, read) {
-  this.title = title;
-  this.author = author;
-  this.numberOfPages = numberOfPages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, numberOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+  }
 
-Book.prototype.toggleStatus = function() {
-  this.read = !this.read;
+  toggleStatus() {
+    this.read = !this.read;
+  }
 }
 
 function displayLibrary() {
